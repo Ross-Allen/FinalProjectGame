d193090eb3f13607cb537652b9a1db359d8723a1
Ross Allen <r.dallen.dev@gmail.com>
2018-08-13 16:06:25 -0600

    Added correct image path that renders at runtime

--- JavaFXGame/src/javafxgame/WarScreenController.java
+++ JavaFXGame/src/javafxgame/WarScreenController.java
@@ -7,9 +7,13 @@ package javafxgame;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javax.annotation.Resources;
 
 /**
  * FXML Controller class
@@ -19,19 +23,25 @@ import javafx.scene.image.ImageView;
 public class WarScreenController implements Initializable {
 
     @FXML
-    ImageView warDeck;
+    ImageView warDeck;   
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event){
+        
+    }
     
     
     
-    
-    
-    
+   
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        Image image = new Image("javafxgame/images/back.png");
+        warDeck.setImage(image);
+        
     }    
     
 }
