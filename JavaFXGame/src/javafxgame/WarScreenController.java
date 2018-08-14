b99076323319895d07eb6416dd578ef161edb7c0
Ross Allen <r.dallen.dev@gmail.com>
2018-08-14 15:48:42 -0600

    Added back button handler

--- JavaFXGame/src/javafxgame/WarScreenController.java
+++ JavaFXGame/src/javafxgame/WarScreenController.java
@@ -1,15 +1,21 @@
 package javafxgame;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -221,8 +227,12 @@ public class WarScreenController implements Initializable {
     }//end draw and compare method
     
     @FXML
-    private void handleBackAction(ActionEvent event){
-    
+    private void handleBackAction(ActionEvent event) throws IOException{
+        Parent screen2Parent = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        Scene screen2Scene = new Scene(screen2Parent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(screen2Scene);
+        window.show();
     }
     
     @FXML
